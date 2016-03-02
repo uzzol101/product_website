978353035fec6737a82df3872eb5d9052832c43d
uzzol101 <davepillowgood@gmail.com>
2016-03-02 08:48:55 +0600

    completed whats in section

--- development/js/main.js
+++ development/js/main.js
@@ -18,3 +18,23 @@ $('#myCarousel .item').each(function(){
 });
 
 new WOW().init();
+
+
+var weekDay = ['satarday','sunday','monday','tuesday','wednesday','thirsday','friday'],
+  clockTime = {13:1,14:2,15:3,16:4,17:5,18:6,19:7,20:8,21:9,22:10,23:11,24:12};
+  var date = new Date(),day,hour;
+    day = date.getDay();
+    time = date.getHours();
+    min = date.getMinutes();
+ 
+    console.log('today is : ' + weekDay[day]);
+    if(time > 12){
+      console.log('current time is ' + clockTime[time] +':'+ min +' PM'  );
+    }else{
+      console.log('current time is ' + clockTime[time] +':'+ min +' AM'  );
+    }
+
+ 
+    
+
+
